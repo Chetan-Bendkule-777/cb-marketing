a3c1633c10608ef590ac6a7b2ec1bbb9ee44d61d
Chetan_Bendkule <73456377+Chetan-Bendkule-777@users.noreply.github.com>
2022-09-23 08:58:12 +0530

    CB-2

--- src/compoents/Footer.js
+++ src/compoents/Footer.js
@@ -12,9 +12,7 @@ function Footer() {
     <div className="me-5 d-none d-lg-block">
       <span>Get connected with us on social networks:</span>
     </div>
-    
-
-    
+  
     <div>
       <a href="/" className="me-4 text-reset mx-3">
         <i className="fab fa-facebook-f"></i>
