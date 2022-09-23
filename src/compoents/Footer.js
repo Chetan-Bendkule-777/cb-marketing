2c4ffa6f7c792ecf5a186743a2278b5cfedfcc58
Chetan_Bendkule <73456377+Chetan-Bendkule-777@users.noreply.github.com>
2022-09-23 09:00:15 +0530

    CB-4

--- src/compoents/Footer.js
+++ src/compoents/Footer.js
@@ -3,8 +3,6 @@ import React from 'react'
 function Footer() {
   return (
     <>
-
-
 <footer className="text-center text-lg-start bg-light text-secondary">
  
   <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
